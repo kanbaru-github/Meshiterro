91da2765336068f0ba04e3c3f134c9709260d67a
kanbaru-github <usupfv@gmail.com>
2021-06-15 14:59:26 +0000

    chapter_6_update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6264b4a54118596364b256b51d88e69a671e0baa99a19bbfcf6cbd415a38055610fc6133b8c80f75a4d3ba08aa3677ec053030340d1cd3e066b7220bb6812471'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'homes#top'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  # get 'user/show'
+
+  root 'homes#top'
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resources :post_comments, only: [:create, :destroy]
+   resource :favorites, only: [:create, :destroy]
+ end
+
+ resources :users, only: [:show, :edit, :update]
 
 end
